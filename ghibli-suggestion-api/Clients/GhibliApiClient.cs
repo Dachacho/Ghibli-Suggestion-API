8a0744ede2f7551b068daf022d20edd8edf25689
Dachacho <arevadzedachi123@gmail.com>
2025-08-26 17:26:23 +0400

    feat: added httpclient with get cals

--- ghibli-suggestion-api/Clients/GhibliApiClient.cs
+++ ghibli-suggestion-api/Clients/GhibliApiClient.cs
@@ -1,6 +1,34 @@
+using System.Text.Json;
+using ghibli_suggestion_api.Models;
+
 namespace ghibli_suggestion_api.Clients;
 
 public class GhibliApiClient : IGhibliApiClient
 {
-    
+    private readonly HttpClient _client;
+
+    public GhibliApiClient(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IEnumerable<GhibliFilm>?> GetFilmsAsync()
+    {
+        var response = await _client.GetAsync($"/films");
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<IEnumerable<GhibliFilm>>(content,  
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+
+    public async Task<GhibliFilm?> GetFilmAsync(string id)
+    {
+        var response = await _client.GetAsync($"/films/{id}");
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<GhibliFilm>(content, 
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
 }
--- ghibli-suggestion-api/Clients/IGhibliApiClient.cs
+++ ghibli-suggestion-api/Clients/IGhibliApiClient.cs
@@ -1,5 +1,9 @@
+using ghibli_suggestion_api.Models;
+
 namespace ghibli_suggestion_api.Clients;
 
 public interface IGhibliApiClient
 {
+    Task<IEnumerable<GhibliFilm>?> GetFilmsAsync();
+    Task<GhibliFilm?> GetFilmAsync(string id);
 }
